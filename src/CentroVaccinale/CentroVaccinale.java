4a0255a205536130fef08d4027f9e7fe8e3c4935
Davide Giacomelli <45962903+giacoJack@users.noreply.github.com>
2022-06-01 16:34:54 +0200

    piripillo

--- src/CentroVaccinale/CentroVaccinale.java
+++ src/CentroVaccinale/CentroVaccinale.java
@@ -12,7 +12,7 @@ public class CentroVaccinale {
 	 */
 	public static void main(String[] args) {
 		
-		System.out.print("bella");	//gjkdfgdfhdf
+		System.out.print("bella2");	
 		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
