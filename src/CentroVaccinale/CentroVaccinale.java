6ae55e4a206645e46824590783da04c16e6fcf0d
Gioele Vicini <gioy00gv@gmail.com>
2022-06-01 16:33:29 +0200

    a

--- src/CentroVaccinale/CentroVaccinale.java
+++ src/CentroVaccinale/CentroVaccinale.java
@@ -12,7 +12,7 @@ public class CentroVaccinale {
 	 */
 	public static void main(String[] args) {
 		
-		System.out.print("bella");	
+		System.out.print("bella");	//gjkdfgdfhdf
 		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
